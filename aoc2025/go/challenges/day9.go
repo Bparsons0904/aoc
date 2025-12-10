88b9f92cae2ec8f864f2474fc22efa069332fa72
Bob Parsons <deadstylebp@gmail.com>
2025-12-09 21:10:06 -0600

    working version... ugly but works

--- aoc2025/go/challenges/day9.go
+++ aoc2025/go/challenges/day9.go
@@ -12,6 +12,10 @@ import (
 	logger "github.com/Bparsons0904/goLogger"
 )
 
+type Interval struct {
+	Start, End int
+}
+
 func Day9() {
 	log := logger.New("Day9")
 
@@ -22,77 +26,128 @@ func Day9() {
 	largestAreaPart1 := getLargestArea(redTiles)
 	timer()
 
-	timer = log.Timer("Part 2 Timer getRedAndGreenTiles")
-	redAndGreenTiles := getRedAndGreenTiles(redTiles)
+	timer = log.Timer("Part 2 Timer getInsideIntervals")
+	intervals := getInsideIntervals(redTiles)
 	timer()
 
 	timer = log.Timer("Part 2 Timer")
-	largestAreaPart2 := getLargestAreaWithRedOrGreen(redTiles, redAndGreenTiles)
+	largestAreaPart2 := getLargestAreaWithIntervals(redTiles, intervals)
 	timer()
 
 	log.Info("Results", "Part 1", largestAreaPart1, "Part 2", largestAreaPart2)
 }
 
-func getRedAndGreenTiles(redTiles []grid.Point) *grid.Grid {
-	log := logger.New("Day9").With("Function", "getRedAndGreenTiles")
-	timer := log.Timer("make tile grid")
-	tileGrid := grid.MakeGridByPoints(
-		redTiles,
-		grid.TileDef{Char: '#', Color: grid.ColorRed, Points: redTiles},
-		grid.TileDef{Char: 'O', Color: grid.ColorGreen},
-	)
-	timer()
+func getInsideIntervals(redTiles []grid.Point) map[int][]Interval {
+	log := logger.New("Day9").With("Function", "getInsideIntervals")
 
-	timer = log.Timer("set red and green tiles")
-	for _, point := range redTiles {
-		farthestRight := tileGrid.FindLastObjectToRight(point, '#')
-		if farthestRight != (grid.Point{}) {
-			for i := point.X; i <= farthestRight.X; i++ {
-				tileGrid.SetObject(grid.Point{X: i, Y: point.Y}, 'O')
+	type VerticalEdge struct {
+		X          int
+		MinY, MaxY int
+	}
+	type HorizontalEdge struct {
+		Y          int
+		MinX, MaxX int
+	}
+
+	var verticalEdges []VerticalEdge
+	var horizontalEdges []HorizontalEdge
+
+	timer := log.Timer("build edges")
+	for i := range redTiles {
+		current := redTiles[i]
+		next := redTiles[(i+1)%len(redTiles)]
+
+		if current.X == next.X {
+			minY, maxY := current.Y, next.Y
+			if minY > maxY {
+				minY, maxY = maxY, minY
 			}
-		}
-		farthestDown := tileGrid.FindLastObjectToBottom(point, '#')
-		if farthestDown != (grid.Point{}) {
-			for i := point.Y; i <= farthestDown.Y; i++ {
-				tileGrid.SetObject(grid.Point{X: point.X, Y: i}, 'O')
+			verticalEdges = append(
+				verticalEdges,
+				VerticalEdge{X: current.X, MinY: minY, MaxY: maxY},
+			)
+		} else {
+			minX, maxX := current.X, next.X
+			if minX > maxX {
+				minX, maxX = maxX, minX
 			}
+			horizontalEdges = append(horizontalEdges, HorizontalEdge{Y: current.Y, MinX: minX, MaxX: maxX})
 		}
 	}
 	timer()
 
-	timer = log.Timer("set outer tiles")
-OuterLoop:
-	for i, point := range tileGrid.Map {
-		var farthestRight grid.Point
-
-		for j, char := range point {
-			if char == 'O' && farthestRight == (grid.Point{}) {
-				farthestRight = tileGrid.FindLastObjectToRight(grid.Point{X: j, Y: i}, 'O')
-			}
-			if farthestRight != (grid.Point{}) {
-				if j == farthestRight.X {
-					continue OuterLoop
-				}
-				tileGrid.SetObject(grid.Point{X: j, Y: i}, 'O')
-			}
+	minY, maxY := redTiles[0].Y, redTiles[0].Y
+	for _, p := range redTiles {
+		if p.Y < minY {
+			minY = p.Y
+		}
+		if p.Y > maxY {
+			maxY = p.Y
 		}
 	}
-	tileGrid.Print()
 
+	timer = log.Timer("build intervals")
+	intervals := make(map[int][]Interval)
+
+	for y := minY; y <= maxY; y++ {
+		var crossings []int
+
+		for _, edge := range verticalEdges {
+			if y >= edge.MinY && y < edge.MaxY {
+				crossings = append(crossings, edge.X)
+			}
+		}
+
+		slices.Sort(crossings)
+
+		var rowIntervals []Interval
+		for i := 0; i+1 < len(crossings); i += 2 {
+			rowIntervals = append(rowIntervals, Interval{Start: crossings[i], End: crossings[i+1]})
+		}
+
+		for _, edge := range horizontalEdges {
+			if edge.Y == y {
+				rowIntervals = append(rowIntervals, Interval{Start: edge.MinX, End: edge.MaxX})
+			}
+		}
+
+		if len(rowIntervals) > 0 {
+			intervals[y] = mergeIntervals(rowIntervals)
+		}
+	}
 	timer()
 
-	return tileGrid
+	return intervals
 }
 
-func getLargestAreaWithRedOrGreen(redTiles []grid.Point, fullGrid *grid.Grid) int {
-	log := logger.New("Day9").With("Function", "getLargestAreaWithRedOrGreen")
-	timer := log.Timer("get potential tiles")
-	type potentialTile struct {
-		point1 grid.Point
-		point2 grid.Point
-		result int
+func mergeIntervals(intervals []Interval) []Interval {
+	if len(intervals) == 0 {
+		return nil
 	}
-	potentialTiles := make([]potentialTile, 0, len(redTiles)*len(redTiles))
+
+	slices.SortFunc(intervals, func(a, b Interval) int {
+		return a.Start - b.Start
+	})
+
+	merged := []Interval{intervals[0]}
+	for i := 1; i < len(intervals); i++ {
+		last := &merged[len(merged)-1]
+		curr := intervals[i]
+
+		if curr.Start <= last.End+1 {
+			if curr.End > last.End {
+				last.End = curr.End
+			}
+		} else {
+			merged = append(merged, curr)
+		}
+	}
+
+	return merged
+}
+
+func getLargestAreaWithIntervals(redTiles []grid.Point, intervals map[int][]Interval) int {
+	log := logger.New("Day9").With("Function", "getLargestAreaWithIntervals")
 
 	abs := func(x int) int {
 		if x < 0 {
@@ -100,55 +155,75 @@ func getLargestAreaWithRedOrGreen(redTiles []grid.Point, fullGrid *grid.Grid) in
 		}
 		return x
 	}
-	for _, point1 := range redTiles {
-		for _, point2 := range redTiles {
-			width := abs(point1.X-point2.X) + 1
-			height := abs(point1.Y-point2.Y) + 1
-			area := width * height
-			potentialTiles = append(
-				potentialTiles,
-				potentialTile{point1: point1, point2: point2, result: area},
-			)
+
+	timer := log.Timer("get potential tiles")
+	type potentialTile struct {
+		point1, point2 grid.Point
+		area           int
+	}
+	potentialTiles := make([]potentialTile, 0, len(redTiles)*len(redTiles))
+
+	for _, p1 := range redTiles {
+		for _, p2 := range redTiles {
+			if p1 == p2 {
+				continue
+			}
+			width := abs(p1.X-p2.X) + 1
+			height := abs(p1.Y-p2.Y) + 1
+			potentialTiles = append(potentialTiles, potentialTile{
+				point1: p1,
+				point2: p2,
+				area:   width * height,
+			})
 		}
 	}
-
 	timer()
 
 	timer = log.Timer("sort potential tiles")
 	slices.SortFunc(potentialTiles, func(a, b potentialTile) int {
-		return int(b.result - a.result)
+		return b.area - a.area
 	})
-
 	timer()
 
-OuterLoop:
-	for _, potential := range potentialTiles {
-		// timer = log.Timer("check potential tile")
-		minX := potential.point1.X
-		maxX := potential.point2.X
-		if potential.point2.X < minX {
+	timer = log.Timer("check rectangles")
+	for _, pt := range potentialTiles {
+		minX, maxX := pt.point1.X, pt.point2.X
+		if minX > maxX {
 			minX, maxX = maxX, minX
 		}
-		minY := potential.point1.Y
-		maxY := potential.point2.Y
-		if potential.point2.Y < minY {
+		minY, maxY := pt.point1.Y, pt.point2.Y
+		if minY > maxY {
 			minY, maxY = maxY, minY
 		}
 
-		for i := minY; i <= maxY; i++ {
-			for j := minX; j <= maxX; j++ {
-				if fullGrid.Map[i][j] != 'O' {
-					// timer()
-					continue OuterLoop
-				}
-			}
+		if isRectangleInside(minX, maxX, minY, maxY, intervals) {
+			timer()
+			return pt.area
 		}
-
-		// timer()
-		return int(potential.result)
 	}
 
 	return 0
+}
+
+func isRectangleInside(minX, maxX, minY, maxY int, intervals map[int][]Interval) bool {
+	for y := minY; y <= maxY; y++ {
+		rowIntervals, exists := intervals[y]
+		if !exists {
+			return false
+		}
+
+		contained := false
+		for _, iv := range rowIntervals {
+			if iv.Start <= minX && maxX <= iv.End {
+				contained = true
+				break
+			}
+		}
+		if !contained {
+			return false
+		}
+	}
+	return true
 }
 
 func getLargestArea(redTiles []grid.Point) int {
